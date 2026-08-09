ab29f4c3e3e1e2b4b26c39e21826b663d96218ff
Rukiya Nus Ha <rukiyanusha123@gmail.com>
2026-08-09 11:45:44 +0530

    Solve array problems

---
+++ Two/script.js
@@ -0,0 +1,8 @@
+for(let i=1;i<=50;i++){
+    console .log(i)
+}
+////////////////////////////
+let marks=[78,89,45,76,100,35,47,50,77,68]
+
+let result=marks.filter(mark=>mark>50);
+console.log(result)
